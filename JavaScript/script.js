d65e73385dda038b9ee936f216d055f1e37e04e3
KhairUnNisa10 <49787331+KhairUnNisa10@users.noreply.github.com>
2021-03-02 23:40:41 +0500

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -32,28 +32,74 @@ function greetings(){
 //JSON For Questions
 const quizQues = [
 	{
-		question: "Q1: Who invented HTML?",
-		a: "Tim Berners Lee",
-		b: "Charles Babbage",
-		c: "John Doe",
-		d: "None of the Above",
+		question: "Q1: python is a:",
+		a: "high level programming language",
+		b: "assembly language",
+		c: "low level programming language",
 		ans: "ans1"
 	},
 	{
-		question: "Q2: What is the full form of HTML?",
-		a: "Hyper Text Mark Language",
-		b: "Hyper Text Markup Language",
-		c: "Hyper Text Markup Languages",
-		d: "None of the Above",
+		question: "Q2: Who invented python:",
+		a: "Guido van Rossum",
+		b: "James gosling",
+		c: "Rasmus lerdorf",
+		ans: "ans1"
+	},
+	{
+		question: "Q3: Which one is illegal variable name:",
+		a: "MYVAR",
+		b: "MY_var",
+		c: "my var",
+		ans: "ans3"
+	},
+	{
+		question: "Q4: python code is run by: ",
+		a: "compiler",
+		b: "interpreter",
+		c: "none of the above",
 		ans: "ans2"
 	},
 	{
-		question: "Q3: What is the full form of CSS?",
-		a: "Cascading Style Sheet",
-		b: "Cascading Styling Sheet",
-		c: "Cascading Style Sheets",
-		d: "None of the Above",
+		question: "Q5: Can we use the “else” clause for loops",
+		a: "yes",
+		b: "no",
+		c: "rarely",
+		ans: "ans1"
+	},
+	{
+		question: "Q6:  What is the output of the following code? \nstr = 'pynative' \nprint (str[1:3])",
+		a: "py",
+		b: "pyn",
+		c: "yn",
 		ans: "ans3"
+	},
+	{
+		question: "Q7:  What is the output of the following code? \nvalueOne = 5 ** 2\n valueTwo = 5 ** 3\n print(valueOne)\nprint(valueTwo)",
+		a: "10 15",
+		b: "25 125",
+		c: "Error: invalid syntax",
+		ans: "ans2"
+	},
+	{
+		question: "Q8: What is the output of the following code?\n var1 = 1\n var2 = 2\n var3 = '3'\n print(var1 + var2 + var3)",
+		a: "6",
+		b: "123",
+		c: "Error. Mixing operators between numbers and strings are not supported",
+		ans: "ans3"
+	},
+	{
+		question: "Q9: What is the Output of the following code?\n for x in range(0.5, 5.5, 0.5):\n print(x)",
+		a: "[0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5]",
+		b: "[0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]",
+		c: "The Program executed with errors",
+		ans: "ans2"
+	},
+	{
+		question: "Q10: What is the output of the following code? \nfor i in range(10, 15, 1):\n print( i, end=', ')",
+		a: "10, 11, 12, 13, 14",
+		b: "10, 11, 12, 13, 14, 15",
+		c: "10,12,14",
+		ans: "ans1"
 	}
 ];
 
